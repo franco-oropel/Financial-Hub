014615c82001ad9d3c218fce7f41cd388c4c7d1f
Franco Oropel <francooropel00@gmail.com>
2024-08-26 13:10:59 +0200

    Initial config of Java project with Spring Boot

---
+++ src/test/java/com/financialhub/app/FinancialHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financialhub.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
